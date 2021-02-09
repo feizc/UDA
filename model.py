342f8ff83a9b246b4df92ba6bed2f7ec4da50a25
zcfei <3300949577@qq.com>
2021-02-09 21:24:32 +0800

    model

--- model.py
+++ model.py
@@ -1,5 +1,11 @@
-from transformers import XLNetTokenizer, XLNetLMHeadModel 
+from transformers import * 
 import torch 
+import torch.nn as nn 
+from torch.nn import CrossEntropyLoss
 
-tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased')
-model = XLNetLMHeadModel.from_pretrained('xlnet-base-cased')
+#tokenizer = XLNetTokenizer.from_pretrained('xlnet-base-cased')
+#model = XLNetLMHeadModel.from_pretrained('xlnet-base-cased') 
+
+
+class UDA()
+
